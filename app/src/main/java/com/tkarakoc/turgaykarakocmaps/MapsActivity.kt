3c97ec5316bc5fc6d6fd1924ae7dbac27ef1db0d
trgykrk <81322526+trgykrk@users.noreply.github.com>
2021-04-13 14:48:00 +0300

    fourth update push

--- app/src/main/java/com/tkarakoc/turgaykarakocmaps/MapsActivity.kt
+++ app/src/main/java/com/tkarakoc/turgaykarakocmaps/MapsActivity.kt
@@ -1,12 +1,16 @@
 package com.tkarakoc.turgaykarakocmaps
 
+import android.Manifest
 import android.content.Context
+import android.content.pm.PackageManager
 import android.location.Geocoder
 import android.location.Location
 import android.location.LocationListener
 import android.location.LocationManager
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.core.app.ActivityCompat
+import androidx.core.content.ContextCompat
 
 import com.google.android.gms.maps.CameraUpdateFactory
 import com.google.android.gms.maps.GoogleMap
@@ -25,7 +29,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_maps)
-        // Obtain the SupportMapFragment and get notified when the map is ready to be used.
+
         val mapFragment = supportFragmentManager
                 .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
@@ -35,12 +39,12 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
     override fun onMapReady(googleMap: GoogleMap) {
         mMap = googleMap
 
-        // Add a marker in Sydney and move the camera
+
         val sydney = LatLng(-34.0, 151.0)
         /*val ankara = LatLng(39.9208372, 32.8454561)
         mMap.addMarker(MarkerOptions().position(ankara).title("Marker in Ankara"))
         mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(ankara, 15f)) */
-        
+
         mMap.addMarker(MarkerOptions().position(sydney).title("Marker in Sydney"))
         mMap.moveCamera(CameraUpdateFactory.newLatLng(sydney))
 
@@ -58,15 +62,31 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
                 try {
 
                     val addressList = geocoder.getFromLocation(p0.latitude, p0.longitude, 1)
-                    if(addressList.size > 0) {
+                    if (addressList.size > 0) {
                         println(addressList.get(0).toString())
                     }
-                }catch (e: Exception){
+                } catch (e: Exception) {
                     e.printStackTrace()
                 }
             }
 
         }
+        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+            //izin verilmemiş
+            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
 
+        } else {
+            //izin verilmiş
+            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1f, locationListener)
+            val sonBilinenKonum = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
+            if (sonBilinenKonum != null) {
+                val sonBilinenLatLng = LatLng(sonBilinenKonum.latitude, sonBilinenKonum.longitude)
+                mMap.addMarker(MarkerOptions().position(sonBilinenLatLng).title("Son Bilinen Konumunuz"))
+                mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sonBilinenLatLng, 15f))
+            }
+        }
     }
 }
+
+
+
