60e9895c08997033f1ac4dd82813075f98fc6200
trgykrk <81322526+trgykrk@users.noreply.github.com>
2021-04-20 13:24:50 +0300

    fifth update push

--- app/src/main/java/com/tkarakoc/turgaykarakocmaps/MapsActivity.kt
+++ app/src/main/java/com/tkarakoc/turgaykarakocmaps/MapsActivity.kt
@@ -31,7 +31,7 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
         setContentView(R.layout.activity_maps)
 
         val mapFragment = supportFragmentManager
-                .findFragmentById(R.id.map) as SupportMapFragment
+            .findFragmentById(R.id.map) as SupportMapFragment
         mapFragment.getMapAsync(this)
     }
 
@@ -71,22 +71,58 @@ class MapsActivity : AppCompatActivity(), OnMapReadyCallback {
             }
 
         }
-        if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED) {
+        if (ContextCompat.checkSelfPermission(
+                this,
+                Manifest.permission.ACCESS_FINE_LOCATION
+            ) != PackageManager.PERMISSION_GRANTED
+        ) {
             //izin verilmemiş
-            ActivityCompat.requestPermissions(this, arrayOf(Manifest.permission.ACCESS_FINE_LOCATION), 1)
+            ActivityCompat.requestPermissions(
+                this,
+                arrayOf(Manifest.permission.ACCESS_FINE_LOCATION),
+                1
+            )
 
         } else {
             //izin verilmiş
-            locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1f, locationListener)
+            locationManager.requestLocationUpdates(
+                LocationManager.GPS_PROVIDER,
+                1,
+                1f,
+                locationListener
+            )
             val sonBilinenKonum = locationManager.getLastKnownLocation(LocationManager.GPS_PROVIDER)
             if (sonBilinenKonum != null) {
                 val sonBilinenLatLng = LatLng(sonBilinenKonum.latitude, sonBilinenKonum.longitude)
-                mMap.addMarker(MarkerOptions().position(sonBilinenLatLng).title("Son Bilinen Konumunuz"))
+                mMap.addMarker(
+                    MarkerOptions().position(sonBilinenLatLng).title("Son Bilinen Konumunuz")
+                )
                 mMap.moveCamera(CameraUpdateFactory.newLatLngZoom(sonBilinenLatLng, 15f))
             }
         }
+
+     }
+    override fun onRequestPermissionsResult(
+            requestCode: Int,
+            permissions: Array<out String>,
+            grantResults: IntArray
+    ) {
+
+        if(requestCode == 1) {
+            if(grantResults.size > 0) {
+                if(ContextCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) == PackageManager.PERMISSION_GRANTED) {
+                    // izin verildi
+                    locationManager.requestLocationUpdates(LocationManager.GPS_PROVIDER, 1, 1f, locationListener)
+                }
+            }
+        }
+
+        super.onRequestPermissionsResult(requestCode, permissions, grantResults)
     }
+
+
 }
+
 
 
 
